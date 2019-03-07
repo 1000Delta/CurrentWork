3a6b52816d146669f6fe41a4513821751eccdea8
1000Delta <13975001197@163.com>
2019-03-07 13:30:14 +0800

    feat: 添加搜索api

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use SE\SEQuery\SEData\SEDataLink;
+use SE\SEQuery\SEQuery;
+
 /**
  * Class SearchController
  * 搜索控制器，负责处理搜索行为
@@ -18,8 +21,26 @@ class SearchController extends Controller
     {
         //
     }
-
-    public function search() {
     
+    /**
+     * @param string $key 搜索关键字
+     * @param int $page 搜索页码，从0开始
+     * @return array 返回查询数据
+     */
+    public function search($key, $page = 0) {
+    
+        // 创建搜索映射
+        /**
+         * @var array 网站搜索映射
+         */
+        $sMap = SEDataLink::getMap($key);
+        $query = new SEQuery('my_index', 'my_type');
+        if ($page < 1) {
+            
+            return $query->search(env('ES_PAGE_SIZE'), $sMap);
+        } else {
+            
+            return $query->pageSearch($page*env('ES_PAGE_SIZE')-1, env('ES_PAGE_SIZE'), $sMap);
+        }
     }
 }

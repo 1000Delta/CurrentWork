3cb4624efb138b8afc1008f67a2d1e039a102a46
1000Delta <13975001197@163.com>
2019-04-05 00:57:21 +0800

    fix: 修改phpDocumenter变量名与类型顺序

--- app/Class/SE/SEQuery/SEDataReader.php
+++ app/Class/SE/SEQuery/SEDataReader.php
@@ -12,22 +12,22 @@ interface SEDataReader {
     
     /**
      * 搜索接口
-     * @param $size int 显示搜索结果数
-     * @param $keyMap array 搜索字段与关键字组成的关联数组
+     * @param int $size 显示搜索结果数
+     * @param string $key 搜索字段与关键字组成的关联数组
      * @return array 搜索结果列表
      *
      * 提供搜索功能
      */
-    public function search($size, $keyMap);
+    public function search(int $size, string $key);
     
     /**
      * 分页搜索
-     * @param $from int 搜索开始的位置
-     * @param $size int 搜索显示的条目
-     * @param $keyMap array 字段-关键字映射
+     * @param int $from 搜索开始的位置
+     * @param int $size 搜索显示的条目
+     * @param string $key 搜索关键字
      * @return array 搜索结果列表
      *
      * 提供分页搜索功能
      */
-    public function pageSearch($from, $size, $keyMap);
+    public function pageSearch(int $from, int $size, string $key);
 }
--- app/Class/SE/SEQuery/SEDataWriter.php
+++ app/Class/SE/SEQuery/SEDataWriter.php
@@ -9,27 +9,25 @@
 namespace SE\SEQuery;
 
 
-use SE\SEQuery\SEData\ISEData;
-
 interface SEDataWriter {
 
     /**
      * 写入数据
-     * @param $data ISEData 数据对象
+     * @param array $data 数据对象
      * @return int 存入数据的索引
      */
     public function add($data);
     
     /**
      * 修改数据项
-     * @param $id int 需要修改的数据项索引
-     * @param $data ISEData 数据对象
+     * @param string $id 需要修改的数据项索引
+     * @param array $data 数据对象
      */
     public function mod($id, $data);
     
     /**
      * 删除数据
-     * @param $id ISEData 数据对象
+     * @param string $id 数据索引
      */
     public function del($id);
 }

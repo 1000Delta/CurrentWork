768fbe9a08badf0a9e8cc8b096312eac72d6e75f
1000Delta <13975001197@163.com>
2019-04-15 21:50:20 +0800

    chore: 转移api文档生成模式php -> yaml

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,11 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+
+use OpenApi\Annotations as OA;
+
 /**
- * @var \Laravel\Lumen\Routing\Router $router
+ * @var Laravel\Lumen\Routing\Router $router
  */
 
 $router->get('/', function () use ($router) {
@@ -20,24 +23,107 @@ $router->get('/', function () use ($router) {
 });
 
 /**
- * @SWG\Swagger(
- *     swagger="2.0"
- *     schemes={"https"},
- *     host="localhost",
- *     basePath="/"
- *     @SWG\Info(
+ * API文档标题
+ * @OA\OpenApi(
+ *     openapi="3.0.0",
+ *     @OA\Server(
+ *         url="http://localhost",
+ *         description="API 服务器"
+ *     ),
+ *     @OA\Info(
  *         version="1.0.0",
  *         title="湘大文库API",
  *         description="描述和测试湘大文库API"
  *     )
  * )
+ *
  */
 
 /**
+ * API标签分类
+ * @OA\Tag(
+ *     name="search",
+ *     description="搜索相关"
+ * )
  *
+ * @OA\Tag(
+ *     name="update",
+ *     description="更新数据相关"
+ * )
+ *
+ * @OA\Tag(
+ *     name="external",
+ *     description="额外接口"
+ * )
+ */
+
+/**
+ * @OA\Get(
+ *     path="/search/{key}",
+ *     tags={"search"},
+ *     summary="分页搜索",
+ *     description="返回查询到的页面列表",
+ *     operationId="searchWebPage",
+ *     @OA\Parameter(
+ *         name="key",
+ *         in="path",
+ *         description="要搜索的关键词/句",
+ *         required=true,
+ *         @OA\Schema(
+ *             type="string",
+ *             default="关键字"
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="查询成功",
+ *         @OA\JsonContent(
+ *             type="array",
+ *             @OA\Items(
+ *                 ref="#"
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=400,
+ *         description="查询失败"
+ *     )
+ * )
  */
 $router->get('/search/{key}[/{page}]', 'SearchController@search');
 
+
+/**
+ * @OA\Post(
+ *     path="/update",
+ *     tags={"update"},
+ *     summary="更新页面",
+ *     description="更新页面存储",
+ *     operationId="update",
+ *     @OA\Parameter(
+ *         name="data",
+ *         in="query",
+ *         description="要更新的数据条目",
+ *         required=true,
+ *         @OA\Schema(
+ *             type="json",
+
+ *             default="{'text': '','keyword':{},'note': '','picurl': '','tags': {},'title': {},'url': {}}",
+ *             description="数据项json字符串",
+ *         ),
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="查询成功",
+ *         @OA\JsonContent(
+ *             type="array",
+ *             @OA\Items(
+ *                 ref="#"
+ *             )
+ *         )
+ *     )
+ * )
+ */
 $router->post('/update', 'UpdateController@update');
 
 $router->group(['middleware' => 'CorsMiddleware'], function () use ($router) {
@@ -45,7 +131,4 @@ $router->group(['middleware' => 'CorsMiddleware'], function () use ($router) {
     $router->get('/test', 'UpdateController@test');
 });
 
-/**
- * 返回代码 return code
- *
- */
+$router->get('/doc', 'SwaggerController@doc');
